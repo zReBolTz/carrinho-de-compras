a50e9140237bf9e478c42e0326191faab6467a9e
zReBolTz <ferreiraw36@gmail.com>
2025-02-11 20:13:24 -0300

    feat: finalizando componente header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,23 @@
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div>component header</div>;
+  return (
+    <header className="w-full px-1  bg-slate-200">
+      <nav className="w-full max-w-7xl flex justify-between h-14 items-center px-5 mx-auto">
+        <Link to="/" className="font-bold text-2xl">
+          <p>Dev Shop</p>
+        </Link>
+
+        <Link to="/cart" className="relative">
+          <ShoppingCart />
+          <span className="bg-blue-400  w-5 h-5 rounded absolute flex justify-center items-center -top-2 -right-2">
+            1
+          </span>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
