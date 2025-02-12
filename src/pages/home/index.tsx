b9e6c65c68a52f7b27e9f812d54be60c7ba01a89
zReBolTz <ferreiraw36@gmail.com>
2025-02-11 21:47:42 -0300

    feat: Fazendo requisição e renderizando na tela

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,31 @@
 import { ShoppingCart } from "lucide-react";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 
 const Home = () => {
+  interface productsProps {
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+  }
+  const [products, setProducts] = useState<productsProps[]>([]);
+  useEffect(() => {
+    async function getProduct() {
+      const response = await api.get("/products");
+
+      const productData = response.data.map((product: any) => ({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        image: product.image,
+      }));
+
+      setProducts(productData);
+    }
+
+    getProduct();
+  }, []);
   return (
     <div>
       <main className="w-full max-w-7xl mx-auto px-4 ">
@@ -8,20 +33,27 @@ const Home = () => {
           Produtos em Alta
         </h1>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              src="https://m.media-amazon.com/images/I/416ZUxb5TiL._AC_UF1000,1000_QL80_.jpg"
-              alt="Logo Produto"
-              className="w-full rounded-lg max-h-70 mb-2"
-            />
-            <p className="font-medium mt-1 mb-2">AirPods (3.ª geração)</p>
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">R$ 1.500,00</strong>
-              <span className="bg-zinc-900 rounded p-1">
-                <ShoppingCart size={20} color="#fff" />
-              </span>
-            </div>
-          </section>
+          {products.map((product) => (
+            <section className="w-full" key={product.id}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="w-full rounded-lg max-h-70 mb-2"
+              />
+              <p className="font-medium mt-1 mb-2">{product.title}</p>
+              <div className="flex gap-3 items-center">
+                <strong className="text-zinc-700/90">
+                  {product.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </strong>
+                <span className="bg-zinc-900 rounded p-1">
+                  <ShoppingCart size={20} color="#fff" />
+                </span>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </div>
