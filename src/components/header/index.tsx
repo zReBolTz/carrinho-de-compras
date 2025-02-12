f879ce842e0d63fbc73b5e6aa33526607b500fc8
zReBolTz <ferreiraw36@gmail.com>
2025-02-12 20:40:59 -0300

    feat: fazendo renderização condicional com cart

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import { ShoppingCart } from "lucide-react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../../context/cartContext";
 
 const Header = () => {
+  const { cart } = useContext(CartContext);
   return (
     <header className="w-full px-1  bg-slate-200">
       <nav className="w-full max-w-7xl flex justify-between h-14 items-center px-5 mx-auto">
@@ -11,9 +14,11 @@ const Header = () => {
 
         <Link to="/cart" className="relative">
           <ShoppingCart />
-          <span className="bg-blue-400  w-5 h-5 rounded absolute flex justify-center items-center -top-2 -right-2">
-            1
-          </span>
+          {cart.length > 0 && (
+            <span className="bg-blue-400  w-5 h-5 rounded absolute flex justify-center items-center -top-2 -right-2">
+              {cart.length}
+            </span>
+          )}
         </Link>
       </nav>
     </header>
