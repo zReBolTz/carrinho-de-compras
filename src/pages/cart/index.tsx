aec37f590204a924d4dc1cae36e5147feac6bdc7
zReBolTz <ferreiraw36@gmail.com>
2025-02-12 21:38:49 -0300

    feat: renderizando carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,26 +1,42 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/cartContext";
+
 const Cart = () => {
+  const { cart } = useContext(CartContext);
   return (
     <div className="w-full max-w-7xl mx-auto ">
       <h1 className="font-bold text-2xl text-center my-4">Meu Carrinho</h1>
-      <section className="flex items-center justify-between border-b-2 border-gray-300">
-        <img
-          src="https://down-tw.img.susercontent.com/file/tw-11134207-7rasg-m46uqw7b1lds07"
-          alt="Logo Produto"
-          className="w-28"
-        />
-        <strong>R$ 1.500,00</strong>
+      {cart.map((item) => (
+        <section
+          key={item.id}
+          className="flex items-center justify-between border-b-2 border-gray-300"
+        >
+          <img src={item.image} alt="Logo Produto" className="w-28" />
+          <strong>
+            {item.price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </strong>
 
-        <div className="flex gap-3 items-center justify-center">
-          <button className="px-2 bg-slate-600 text-white rounded font-meidum flex items-center justify-center">
-            -
-          </button>
-          1
-          <button className="px-2 bg-slate-600 text-white rounded font-meidum flex items-center justify-center">
-            +
-          </button>
-        </div>
-        <strong>SubTotal: 1.500,00</strong>
-      </section>
+          <div className="flex gap-3 items-center justify-center">
+            <button className="px-2 bg-slate-600 text-white rounded font-meidum flex items-center justify-center">
+              -
+            </button>
+            {item.amount}
+            <button className="px-2 bg-slate-600 text-white rounded font-meidum flex items-center justify-center">
+              +
+            </button>
+          </div>
+          <strong>
+            SubTotal:{" "}
+            {item.total.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </strong>
+        </section>
+      ))}
       <p className="font-bold mt-4">Total: R$1.500,00</p>
     </div>
   );
